a8ee1f953e05f9adaf3d00a607ac22cb0c21849d
jusunj <jusun@postlabs.io>
2022-11-17 18:01:46 +0900

    텍스트 에러 수정

--- src/page/LoadingPage.jsx
+++ src/page/LoadingPage.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import {Header32, MonoBlack} from "../styledMixins"
+import React, { useState, useEffect } from "react";
+import { Header32, MonoBlack } from "../styledMixins";
 import styled from "styled-components";
 import GNB from "../components/GNB";
 import Dot from "../components/Dot";
@@ -9,28 +9,30 @@ const LoadingPage = () => {
   const [circle1, setCircle1] = useState(false);
   const [circle2, setCircle2] = useState(false);
   const [circle3, setCircle3] = useState(false);
-  useEffect(()=>{
-    let timer = setInterval(()=>{ setCount(count + 1)}, 200);
-    
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setCount(count + 1);
+    }, 200);
+
     return () => {
       clearInterval(timer);
-    }
+    };
   });
 
-  useEffect(()=>{
-    if( count % 4 === 0){
+  useEffect(() => {
+    if (count % 4 === 0) {
       setCircle1(false);
       setCircle2(false);
       setCircle3(false);
-    }else if( count % 4 === 1){
+    } else if (count % 4 === 1) {
       setCircle1(true);
       setCircle2(false);
       setCircle3(false);
-    }else if( count % 4 === 2){
+    } else if (count % 4 === 2) {
       setCircle1(true);
       setCircle2(true);
       setCircle3(false);
-    }else if( count % 4 === 3){
+    } else if (count % 4 === 3) {
       setCircle1(true);
       setCircle2(true);
       setCircle3(true);
@@ -41,9 +43,9 @@ const LoadingPage = () => {
       <GNB />
       <LoadingBox>
         <DotBox>
-          {circle1 ? <Dot color="#93D0F1"/> : <Dot color="#FFFFFFF"/>}
-          {circle2 ? <Dot color="#F8B3C5"/> : <Dot color="#FFFFFFF"/>}
-          {circle3 ? <Dot color="#FBB562"/> : <Dot color="#FFFFFFF"/>}
+          {circle1 ? <Dot color="#93D0F1" /> : <Dot color="#FFFFFFF" />}
+          {circle2 ? <Dot color="#F8B3C5" /> : <Dot color="#FFFFFFF" />}
+          {circle3 ? <Dot color="#FBB562" /> : <Dot color="#FFFFFFF" />}
         </DotBox>
         <TextBox>
           <Text>아이의 성향을 분석하고 있어요!</Text>
@@ -63,7 +65,6 @@ const StyleListPage = styled.div`
   margin: 0 auto;
   margin-top: 60px;
   //border: 1px solid black;
-  
 `;
 
 const LoadingBox = styled.div`
@@ -74,7 +75,7 @@ const LoadingBox = styled.div`
 `;
 
 const DotBox = styled.div`
-  width: 66px
+  width: 66px;
   height: 14px;
   display: flex;
   flex-direction: row;
@@ -85,7 +86,7 @@ const DotBox = styled.div`
 `;
 
 const TextBox = styled.div`
-  width: 500px
+  width: 500px;
   height: 84px;
 `;
 
